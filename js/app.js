e15fec0df900355d5a814d70ff5825cba8666e87
Kasia Kwiat <kasikkk222@vp.pl>
2016-08-13 14:13:58 +0200

    Walidacja formularza

--- js/app.js
+++ js/app.js
@@ -1,1 +1,68 @@
-$(document).foundation();
+$( document ).ready(function() {
+
+    $("#button-go").click(function(event){
+    event.preventDefault(); 
+    var haslo = $("#my-input").val();
+    var login = "efi";
+    sendAjax(haslo,login);
+      
+  });
+
+
+
+
+   function sendAjax(haslo,login) {
+    
+    var blad = document.getElementById("validation");
+    
+    if(haslo.length>0 && login.length>0){
+     
+          $.ajax({
+          type: "post",
+          data: {
+            login: login,
+            password: haslo
+          },
+          url: "https://efigence-camp.herokuapp.com/api/login",
+          error: function(response) {
+            
+            console.log('er', response.responseText);
+            var obj = JSON.parse(response.responseText);
+            blad.innerHTML= obj.message;
+
+            $("#my-input").css({
+              "border" : "2px solid #f3741f"
+
+            });
+          },
+          success: function(response) {
+            console.log('sukces', response);
+            window.open("http://www.w3schools.com");
+          }
+        });
+     }
+
+      else if((haslo.length==0) || (login.length==0)){
+
+          blad.innerHTML ="Podaj hasło i/lub login";
+           $("#my-input").css({
+              "border" : "2px solid #f3741f"
+            });
+
+        }
+
+         $("#my-input").focus(function(){
+
+            blad.innerHTML=""; 
+            $("#my-input").css({
+              "border": "1px solid #cacaca",
+            });
+            $("#my-input").val("");
+         });
+
+      
+ }
+
+
+
+});
