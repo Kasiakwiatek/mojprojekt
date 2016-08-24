b2888ffef455927cc9ff9e9a77e9b714d5547de6
Kasia Kwiat <kasikkk222@vp.pl>
2016-08-24 12:19:18 +0200

    Struktura i style hedera

--- js/app.js
+++ js/app.js
@@ -1,55 +1,54 @@
+$(document).foundation();
 $( document ).ready(function() {
 
     $("#button-go").click(function(event){
-    event.preventDefault(); 
-    var haslo = $("#my-input").val();
-    var login = "efi";
-    sendAjax(haslo,login);
-      
-  });
-
-
-
+      event.preventDefault(); 
+      var haslo = $("#my-input").val();
+      var login = "efi";
+      sendAjax(haslo,login);
+        
+    });
 
    function sendAjax(haslo,login) {
     
-    var blad = document.getElementById("validation");
-    
-    if(haslo.length>0 && login.length>0){
-     
-          $.ajax({
-          type: "post",
-          data: {
-            login: login,
-            password: haslo
-          },
-          url: "https://efigence-camp.herokuapp.com/api/login",
-          error: function(response) {
-            
-            console.log('er', response.responseText);
-            var obj = JSON.parse(response.responseText);
-            blad.innerHTML= obj.message;
+        var blad = document.getElementById("validation");
+        
+        if(haslo.length && login.length){
+         
+              $.ajax({
+              type: "post",
+              data: {
+                login: login,
+                password: haslo
+              },
+              url: "https://efigence-camp.herokuapp.com/api/login",
 
-            $("#my-input").css({
-              "border" : "2px solid #f3741f"
+              error: function(response) {
+                
+                console.log('er', response.responseText);
+                var obj = JSON.parse(response.responseText);
+                blad.innerHTML= obj.message;
 
+                $("#my-input").css({
+                  "border" : "2px solid #f3741f"
+                });
+              },
+              success: function(response) {
+                console.log('sukces', response);
+                //window.open("http://www.w3schools.com");
+                window.location.assign("file:///C:/Users/Kasia/mojProjekt/dashboard.html");
+              }
             });
-          },
-          success: function(response) {
-            console.log('sukces', response);
-            window.open("http://www.w3schools.com");
-          }
-        });
-     }
+         }
 
-      else if((haslo.length==0) || (login.length==0)){
+          else {
 
-          blad.innerHTML ="Podaj hasło i/lub login";
-           $("#my-input").css({
-              "border" : "2px solid #f3741f"
-            });
+             blad.innerHTML= "Podaj hasło i/lub login";
+               $("#my-input").css({
+                  "border" : "2px solid #f3741f"
+                });
 
-        }
+            }
 
          $("#my-input").focus(function(){
 
@@ -61,7 +60,20 @@ $( document ).ready(function() {
          });
 
       
- }
+    }
+       
+// input search - hide and show
+       $(".input-search").hide();
+       $(".search-icon").on("click", function(){
+          $(".input-search").toggle("slide", { direction: "right" }, 500);
+        });
+
+       $(".input-search").on("blur", function(){
+          $(".input-search").hide("slide", { direction: "right" }, 500);
+        });
+
+       
+
 
 
 
